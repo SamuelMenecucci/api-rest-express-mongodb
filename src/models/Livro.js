edb90f983d6ea7be8891a7a042a5c3c4a536473a
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-06-06 19:34:43 -0300

    criação da entidade livro com o mongoose

---
+++ src/models/Livro.js
@@ -0,0 +1,15 @@
+//arquivo responsável por representar a entidade Livro
+
+import mongoose from "mongoose";
+
+//crio um schema para dize para o mongoose que é uma coleção e passo as atributos que estão no banco. definimos todos os atributos que possuímos no banco, e definimos se são obrigatórios ou não.
+const livroSchema = new mongoose.Schema({
+  id: { type: String },
+  titulo: { type: String, required: true },
+  author: { type: String, required: true },
+  editora: { type: String, required: true },
+  numeroPaginas: { type: Number },
+});
+
+//corresponde a coleção inteira. Passo que no banco a coleção se chama livros, pois é o mesmo nome que coloquei no banco, e passo o schema dessa coleção, que são todos os atributos que a coleção possui e se são obrigatórios ou não.
+export const livros = mongoose.model("livros", livroSchema);
