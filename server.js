f9b576b1c58ffc495ff5d2ebe080511888810b27
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-06-06 15:13:05 -0300

    colocando ideia de rotas dinâmicas

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require("http"); //módulo nativo do node
 const port = 3000;
 
+const rotas = {
+  "/": "Curso de Node",
+  "/livros": "Entrei na página de livros",
+  "/autores": "Listagem de autores",
+  "/editora": "Pag de editora",
+  "/sobre": "Info sobre o projeto",
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Curso de Nodejs");
+  res.end(rotas[req.url]);
 });
 
 server.listen(port, () => {
