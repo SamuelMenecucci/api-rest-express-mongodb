d66771718afd337d51ff99745a91c9e5ea5ff10a
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-06-07 12:21:38 -0300

    passando o atributo author como referencia ao id da entidade Autor, para fazer associação entre schemas

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -6,7 +6,15 @@ import mongoose from "mongoose";
 const livroSchema = new mongoose.Schema({
   id: { type: String },
   titulo: { type: String, required: true },
-  author: { type: String, required: true },
+
+  //ao invés de deixar o tipo do author como string, irei fazer uma referencia para a entidade autor, mais especificamente para o id do autor. deixo como ObjectId pois a referencia é um id de um objeto e não mais uma string
+  //coloco o parametro ref, que é a referencia, dizendo que o id é uma referencia da entidade de autores, que vem do meu model (passo o mesmo nome que está na coleção)
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "autores",
+    required: true,
+  },
+
   editora: { type: String, required: true },
   numeroPaginas: { type: Number },
 });
