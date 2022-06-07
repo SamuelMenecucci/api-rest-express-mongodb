e2a452315c05a4e8256bd1d552555c3201441467
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-06-07 11:46:10 -0300

    anotações

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -12,4 +12,5 @@ const livroSchema = new mongoose.Schema({
 });
 
 //corresponde a coleção inteira. Passo que no banco a coleção se chama livros, pois é o mesmo nome que coloquei no banco, e passo o schema dessa coleção, que são todos os atributos que a coleção possui e se são obrigatórios ou não.
+//caso eu não tenha nenhuma coleção no banco criada, o primeiro parâmetro aqui se refere ao nome da coleção que irá ter no banco, e criará com o mesmo nome que passo aqui
 export const livros = mongoose.model("livros", livroSchema);
